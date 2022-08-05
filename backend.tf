f1f87547a522f508980b5aea1dd5e33226e0d1a9
CKISEMBO <98928239+CKISEMBO@users.noreply.github.com>
2022-08-04 20:04:16 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
  terraform {
    backend "s3" {
-    bucket = "code-sharing-bucket"
-    key    = "avinash/s3file-3.tfstate"
+    bucket = "code-share-bucket"
+    key    = "chriso/s3file-3.tfstate"
     region = "us-east-2"
     profile = "Chriso"
 dynamodb_table= "firstprojecttable"
